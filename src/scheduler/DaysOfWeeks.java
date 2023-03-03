22625862b93b6c696d58bf4be3e82c851e6518ce
League Student <league@iMac.attlocal.net>
2023-03-02 18:29:22 -0800

    :b:
    bruh

--- src/scheduler/DaysOfWeeks.java
+++ src/scheduler/DaysOfWeeks.java
@@ -1,5 +1,14 @@
 package scheduler;
 
-public class DaysOfWeeks {
-
+enum DaysOfWeeks {
+	Sunday(), Monday(), Tuesday(), Wednesday(), Thursday(), Friday(), Saturday();
+	
+	private final LinkedList<Node<String>> schedule;
+	
+	private DaysOfWeeks () {
+		this.schedule = new LinkedList<Node<String>>();
+	}
+	public LinkedList<Node<String>> getLinkedList() {
+		return schedule;
+	}
 }
--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -26,11 +26,77 @@ import java.util.Scanner;
  *   variables.
  */
 public class Scheduler {
-Scanner qAsk = new Scanner(System.in);
-    public static void main(String[] args) {
 
-    }
-    private void setTime(Node<T> n) {
-    	
-    }
+	DaysOfWeeks[] days = DaysOfWeeks.values();
+
+	public static void main(String[] args) {
+		String newResponse = "";
+		Scanner qAsk = new Scanner(System.in);
+		Boolean noClose = true;
+		do {
+			System.out.println("Would you like to close the program? y/n");
+			newResponse = qAsk.next();
+			noClose = newResponse.equals("y") ? true : false;
+			
+
+		} while (noClose);
+	}
+	
+	
+	public static void addNewEvent() {
+		String newResponse = "";
+		Scanner qAsk = new Scanner(System.in);
+		Boolean keepAsking = false;
+		Double time = 1.00;
+		String name = "";
+		Node day = null;
+		Node next;
+		do {
+			System.out.println("Would you like to add to your schedule? y/n");
+			keepAsking = qAsk.next().equals("y") ? true : false;
+
+			if (keepAsking) {
+				System.out.println("enter the day you would like to add to");
+				newResponse = qAsk.next();
+				switch (newResponse) {
+				case "Monday":
+					day = DaysOfWeeks.Monday.getLinkedList().getHead();
+					break;
+				case "Tuesday":
+					day = DaysOfWeeks.Tuesday.getLinkedList().getHead();
+					break;
+				case "Wednesday":
+					day = DaysOfWeeks.Wednesday.getLinkedList().getHead();
+					break;
+				case "Thursday":
+					day = DaysOfWeeks.Thursday.getLinkedList().getHead();
+					break;
+				case "Friday":
+					day = DaysOfWeeks.Friday.getLinkedList().getHead();
+					break;
+				case "Saturday":
+					day = DaysOfWeeks.Saturday.getLinkedList().getHead();
+					break;
+				case "Sunday":
+					day = DaysOfWeeks.Sunday.getLinkedList().getHead();
+					break;
+				}
+				next = day.equals(null) ? null : day.getNext();
+				System.out.println("The time of the event? Format it as 16.30 for 4:30pm");
+				time = Double.parseDouble(qAsk.next());
+				System.out.println("The name of the event?");
+				name = qAsk.next();
+				
+				if(day==null) {
+					day.setValue(name + " " + time.toString());
+				}else {
+					if (day.getValue().toString().contains(time.toString())) {
+						day = next;
+						next = day.getNext();
+						
+					}
+				}
+			}
+		} while (keepAsking);
+	}
 }
